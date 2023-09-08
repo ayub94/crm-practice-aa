ea560856d1b342bc603a3d71132d82fd1c4ea621
Ayubjon Narzulloev <ayubjon94.an@gmail.com>
2023-09-08 11:39:38 +0500

    resolving conflicts

---
+++ src/Crm.BusinessLogic/Services/IStatisticsService.cs
@@ -0,0 +1,17 @@
+namespace Crm.BusinessLogic;
+
+public interface IStatisticsService
+{
+    public int GetClientsCount();
+    {
+        throw new NotImplementedException;
+    }
+    public int GetOrderCount();
+    {
+        throw new NotImplementedException;
+    }
+
+    public int GetOrderCount()
+
+
+}
